69aedcb78a590cbdfe615dda6fb8160ad42d370f
Tylar Overturf <toverturf@gmail.com>
2013-01-30 16:40:40 -0700

    Carbon.

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(LIQUID_BUILD),)
-    all_product_configs := $(shell ls device/*/$(LIQUID_BUILD)/liquid.mk)
+  ifneq ($(CARBON_BUILD),)
+    all_product_configs := $(shell ls device/*/$(CARBON_BUILD)/carbon.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # LIQUID_BUILD
+  endif # CARBON_BUILD
 endif
 
-ifeq ($(LIQUID_BUILD),)
+ifeq ($(CARBON_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
--- envsetup.sh
+++ envsetup.sh
@@ -60,12 +60,12 @@ function check_product()
         return
     fi
 
-    if (echo -n $1 | grep -q -e "^liquid_") ; then
-       LIQUID_BUILD=$(echo -n $1 | sed -e 's/^liquid_//g')
+    if (echo -n $1 | grep -q -e "^carbon_") ; then
+       CARBON_BUILD=$(echo -n $1 | sed -e 's/^carbon_//g')
     else
-       LIQUID_BUILD=
+       CARBON_BUILD=
     fi
-    export LIQUID_BUILD
+    export CARBON_BUILD
 
     CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
         TARGET_PRODUCT=$1 \
@@ -457,7 +457,7 @@ function print_lunch_menu()
     echo
     echo "You're building on" $uname
     echo
-    if [ "z${LIQUID_DEVICES_ONLY}" != "z" ]; then
+    if [ "z${CARBON_DEVICES_ONLY}" != "z" ]; then
        echo "Breakfast menu... pick a combo:"
     else
        echo "Lunch menu... pick a combo:"
@@ -471,7 +471,7 @@ function print_lunch_menu()
         i=$(($i+1))
     done
 
-    if [ "z${LIQUID_DEVICES_ONLY}" != "z" ]; then
+    if [ "z${CARBON_DEVICES_ONLY}" != "z" ]; then
        echo "... and don't forget the bacon!"
     fi
 
@@ -482,7 +482,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka liquid
+        mka carbon
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -493,10 +493,10 @@ function brunch()
 function breakfast()
 {
     target=$1
-    LIQUID_DEVICES_ONLY="true"
+    CARBON_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/liquid/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/carbon/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -512,8 +512,8 @@ function breakfast()
             # A buildtype was specified, assume a full device name
             lunch $target
         else
-            # This is probably just the LIQUID model name
-            lunch liquid_$target-userdebug
+            # This is probably just the CARBON model name
+            lunch carbon_$target-userdebug
         fi
     fi
     return $?
@@ -648,8 +648,8 @@ function tapas()
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=`sed -n -e'/ro\.liquid\.version/s/.*=//p' $OUT/system/build.prop`
-        ZIPFILE=liquid-$MODVERSION.zip
+        MODVERSION=`sed -n -e'/ro\.carbon\.version/s/.*=//p' $OUT/system/build.prop`
+        ZIPFILE=carbon-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
@@ -664,7 +664,7 @@ function eat()
             done
             echo "Device Found.."
         fi
-    if (adb shell cat /system/build.prop | grep -q "ro.liquid.device=$LIQUID_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD");
     then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
@@ -689,7 +689,7 @@ EOF
     fi
     return $?
     else
-        echo "The connected device does not appear to be $LIQUID_BUILD, run away!"
+        echo "The connected device does not appear to be $CARBON_BUILD, run away!"
     fi
 }
 
@@ -1266,7 +1266,7 @@ function lska() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                liquid|otapackage|systemimage)
+                carbon|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
