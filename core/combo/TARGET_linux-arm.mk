1788bb013337d35a247ce5f5bbd725f2c2992f62
winner00 <toverturf@gmail.com>
2013-05-08 21:48:01 -0600

    Compiler Optimizations
    Credit to pbeeler.
    
    Change-Id: Ic83285f8ea541d19e85634553f8fc41956e4d3f2

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -66,15 +66,25 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 ifeq ($(TARGET_USE_O2),true)
-TARGET_arm_CFLAGS :=    -Os \
+TARGET_arm_CFLAGS :=    -O2 \
+                        -fgcse-after-reload \
+                        -fipa-cp-clone \
+                        -fpredictive-commoning \
+                        -fsched-spec-load \
+                        -funswitch-loops \
+                        -fvect-cost-model \
                         -fomit-frame-pointer \
-                        -fstrict-aliasing    \
-                        -fno-tree-vectorize
+                        -fstrict-aliasing \
+                        -Wstrict-aliasing=3 \
+                        -Werror=strict-aliasing
 else
 TARGET_arm_CFLAGS :=    -O3 \
                         -fomit-frame-pointer \
-                        -fstrict-aliasing    \
-                        -funswitch-loops
+                        -fstrict-aliasing \
+                        -Wstrict-aliasing=3 \
+                        -Werror=strict-aliasing \
+                        -funswitch-loops \
+                        -fno-tree-vectorize
 endif
 # Modules can choose to compile some source as thumb. As
 # non-thumb enabled targets are supported, this is treated
@@ -83,16 +93,24 @@ endif
 ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
     ifeq ($(TARGET_USE_O2),true)
     TARGET_thumb_CFLAGS :=  -mthumb \
-                            -O2 \
+                            -Os \
+                            -fgcse-after-reload \
+                            -fipa-cp-clone \
+                            -fpredictive-commoning \
+                            -fsched-spec-load \
+                            -funswitch-loops \
+                            -fvect-cost-model \
                             -fomit-frame-pointer \
-                            -fno-strict-aliasing \
-                            -fno-tree-vectorize
+                            -fstrict-aliasing \
+                            -Wstrict-aliasing=3 \
+                            -Werror=strict-aliasing
     else
     TARGET_thumb_CFLAGS :=  -mthumb \
                             -O3 \
                             -fomit-frame-pointer \
-                            -fno-strict-aliasing \
-                            -fno-tree-vectorize
+                            -fstrict-aliasing \
+                            -Wstrict-aliasing=3 \
+                            -Werror=strict-aliasing
     endif
 else
 TARGET_thumb_CFLAGS := $(TARGET_arm_CFLAGS)
@@ -168,14 +186,25 @@ endif
 TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden $(call cc-option,-std=gnu++11)
 
 # More flags/options can be added here
-TARGET_RELEASE_CFLAGS := \
-			-DNDEBUG \
-			-g \
-			-Wstrict-aliasing=2 \
-			-Werror=strict-aliasing \
-			-fgcse-after-reload \
-			-frerun-cse-after-loop \
-			-frename-registers
+ifndef TARGET_EXTRA_CFLAGS
+  TARGET_RELEASE_CFLAGS := \
+        -DNDEBUG \
+        -g \
+        -Wstrict-aliasing=3 \
+        -Werror=strict-aliasing \
+        -fgcse-after-reload \
+        -frerun-cse-after-loop \
+        -frename-registers
+else
+  TARGET_RELEASE_CFLAGS += \
+        -DNDEBUG \
+        -g \
+        -Wstrict-aliasing=3 \
+        -Werror=strict-aliasing \
+        -fgcse-after-reload \
+        -frerun-cse-after-loop \
+        -frename-registers
+endif
 
 libc_root := bionic/libc
 libm_root := bionic/libm
--- core/combo/select.mk
+++ core/combo/select.mk
@@ -46,15 +46,18 @@ $(combo_target)HAVE_STRLCPY := 0
 $(combo_target)HAVE_STRLCAT := 0
 $(combo_target)HAVE_KERNEL_MODULES := 0
 
-ifneq ($(TARGET_USE_02),true)
 $(combo_target)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
-$(combo_target)RELEASE_CFLAGS := -O2 -g -fno-strict-aliasing
+ifneq ($(TARGET_USE_02),true)
+$(combo_target)RELEASE_CFLAGS := -O2 -fgcse-after-reload -fipa-cp-clone -fpredictive-commoning -fsched-spec-load -funswitch-loops -fvect-cost-model -g -Wstrict-aliasing=3 -Werror=strict-aliasing
 else
-$(combo_target)GLOBAL_CFLAGS := -O3 -g -Wstrict-aliasing=2
+$(combo_target)GLOBAL_CFLAGS := -O3 -g -Wstrict-aliasing=3 -Werror=strict-aliasing
+endif
 ifneq ($(combo_target),HOST_)
-(combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
+$(combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
+else
+$(combo_target)RELEASE_CFLAGS += -Wno-error=strict-aliasing
 endif
-endif
+
 $(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
