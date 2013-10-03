2d5df97b56e9ad939cf7f630d25af7614208cd1b
Chris Soyars <ctso@ctso.me>
2013-10-03 14:32:19 -0700

    Use openssl instead of sha1sum
    Change-Id: Iab8a74d4af6646104cb23312853f811a38bdec0d

--- envsetup.sh
+++ envsetup.sh
@@ -72,7 +72,7 @@ function check_product()
 
     if (echo -n $1 | grep -q -e "^carbon_") ; then
        CARBON_BUILD=$(echo -n $1 | sed -e 's/^carbon_//g')
-       export BUILD_NUMBER=$((date +%s%N ; echo $CARBON_BUILD; hostname) | sha1sum | cut -c1-10)
+       export BUILD_NUMBER=$((date +%s%N ; echo $CARBON_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
     else
        CARBON_BUILD=
     fi
