dfa05bf4646a627bda8ea52d6cd00ba27b780e13
Madina Gbotoe <mgbotoe@gmail.com>
2013-11-16 14:46:57 -0800

    Minionfication
    Make compatible since tracking own /build

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -42,6 +42,6 @@ echo "ro.build.description=$PRIVATE_BUILD_DESC"
 echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
-echo "ro.carbon.device=$CARBON_DEVICE"
+echo "ro.carbon.device=$MINIONS_DEVICE"
 
 echo "# end build properties"
