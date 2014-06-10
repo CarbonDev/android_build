853bceda98a9b45d23a09f725b53523629fec700
PhaseBurn <phaseburn@phaseburn.net>
2014-06-10 03:16:27 -0700

    Fixing adbd so that it's respectful of /data/misc/adb/adb_keys once again. <facepalm>
    Change-Id: I334633b0ef895a740a2749382f311666bfde07b2

--- core/main.mk
+++ core/main.mk
@@ -297,6 +297,7 @@ tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
